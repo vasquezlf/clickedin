e3d3a5ba0d5eea53c074b62ecd1d85c5fb51b683
Lorenzo Q <vasquez.lf@gmail.com>
2018-07-26 15:17:18 -0700

    Backend - login, profile, education, experience, post

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const profiles = require("./routes/api/profiles");
@@ -7,21 +9,32 @@ const posts = require("./routes/api/posts");
 
 const app = express();
 
-// DB Config
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+// DB Config. Extract value of mongoURI from file and save in db
 const db = require("./config/keys").mongoURI;
 
-// Connect to MongoDB
+// Connect to MongoDB via Mongoose
 mongoose
   .connect(db)
   .then(() => console.log("MongoDB Connected"))
   .catch( err => console.log(err));
 
 // Route
-app.get("/", (req, res) => res.send("Hello world"));
+// app.get("/", (req, res) => res.send("Hello world")); //Test
+
+/////// ?? Not sure what's going on here ??
+// Passport middleware
+app.use(passport.initialize());
+// Import passport.js then call passport function from the file
+require("./config/passport")(passport);
+///////
 
 // Use routes
 app.use("/api/users", users);
-app.use("/api/profiles", profiles);
+app.use("/api/profile", profiles);
 app.use("/api/posts", posts);
 
 // Setup for Heroku port
