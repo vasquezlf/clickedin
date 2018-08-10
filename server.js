03233d885581af1a9cad9356af63ffb869903876
Lorenzo Q <vasquez.lf@gmail.com>
2018-08-09 17:38:34 -0700

    Prepare to deploy

--- server.js
+++ server.js
@@ -1,38 +1,42 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const path = require("path")
 
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
+const users = require("./routes/api/users");
+const profiles = require("./routes/api/profiles");
+const posts = require("./routes/api/posts");
 
 const app = express();
 
 // Body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
-// DB Config
-const db = require('./config/keys').mongoURI;
+// DB Config. Extract value of mongoURI from file and save in db
+const db = require("./config/keys").mongoURI;
 
-// Connect to MongoDB
+// Connect to MongoDB via Mongoose
 mongoose
   .connect(db)
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+  .then(() => console.log("MongoDB Connected"))
+  .catch( err => console.log(err));
 
+// Route
+// app.get("/", (req, res) => res.send("Hello world")); //Test
+
+/////// ?? Not sure what's going on here ??
 // Passport middleware
 app.use(passport.initialize());
+// Import passport.js then call passport function from the file
+require("./config/passport")(passport);
+///////
 
-// Passport Config
-require('./config/passport')(passport);
-
-// Use Routes
-app.use('/api/users', users);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
+// Use routes
+app.use("/api/users", users);
+app.use("/api/profile", profiles);
+app.use("/api/posts", posts);
 
 // Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
@@ -44,6 +48,7 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
+// Setup for Heroku port
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
