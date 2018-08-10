29b293a25bc765f642f1c0d3e1a75c98940a2991
Lorenzo Q <vasquez.lf@gmail.com>
2018-08-09 17:32:59 -0700

    Prepare to deploy

--- server.js
+++ server.js
@@ -1,54 +1,49 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const passport = require("passport");
-const path = require("path")
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const path = require('path');
 
-const users = require("./routes/api/users");
-const profiles = require("./routes/api/profiles");
-const posts = require("./routes/api/posts");
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
 
 // Body parser middleware
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// DB Config. Extract value of mongoURI from file and save in db
-const db = require("./config/keys").mongoURI;
+// DB Config
+const db = require('./config/keys').mongoURI;
 
-// Connect to MongoDB via Mongoose
+// Connect to MongoDB
 mongoose
   .connect(db)
-  .then(() => console.log("MongoDB Connected"))
-  .catch( err => console.log(err));
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
-// Route
-// app.get("/", (req, res) => res.send("Hello world")); //Test
-
-/////// ?? Not sure what's going on here ??
 // Passport middleware
 app.use(passport.initialize());
-// Import passport.js then call passport function from the file
-require("./config/passport")(passport);
-///////
 
-// Use routes
-app.use("/api/users", users);
-app.use("/api/profile", profiles);
-app.use("/api/posts", posts);
+// Passport Config
+require('./config/passport')(passport);
 
-// Serve static assets if in production
-if(process.env.NODE_ENV === "production") {
-  // Set a static folder
-  app.user(express.static("client/build"));
-  // get anything that is not an api route
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
-// Setup for Heroku port
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
